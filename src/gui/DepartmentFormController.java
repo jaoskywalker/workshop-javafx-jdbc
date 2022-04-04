83013ae724e5962b21557bcc347faffbc31fc4d8
João Martins <john_victor100@hotmail.com>
2022-04-04 18:41:44 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -20,8 +23,8 @@ import model.services.DepartmentService;
 public class DepartmentFormController implements Initializable {
 
 	private Department entity;
-
 	private DepartmentService service;
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>(); 
 
 	@FXML
 	private TextField txtId;
@@ -37,7 +40,20 @@ public class DepartmentFormController implements Initializable {
 
 	@FXML
 	private Button btCancel;
+	
 
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		if (entity == null) {
@@ -49,9 +65,16 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
 		}
 	}
 
@@ -67,13 +90,6 @@ public class DepartmentFormController implements Initializable {
 		Utils.currentStage(event).close();
 	}
 
-	public void setDepartment(Department entity) {
-		this.entity = entity;
-	}
-
-	public void setDepartmentService(DepartmentService service) {
-		this.service = service;
-	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
